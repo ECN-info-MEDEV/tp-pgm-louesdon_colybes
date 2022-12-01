a12a4ac45ff4b1ea4d2c85a32caea247c1d89479
elouan50 <elouan.colybes@gmail.com>
2022-12-01 11:28:34 +0100

    fix structure

---
+++ MEDEV01122022/src/main/java/edu/centralenantes/projetpgm/ProjetPGM.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package edu.centralenantes.projetpgm;
+
+/**
+ *
+ * @author aulou
+ */
+public class ProjetPGM {
+
+    public static void main(String[] args) {
+        Image im = new Image();
+        im.ReadFile("Test");
+    }
+}
